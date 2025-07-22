9ec0d8280ccd312e0069da5b8d8f4ff887ba1f8a
Yodaman07 <ayaan.irshad@gmail.com>
2025-07-22 14:40:04 -0700

    fixed comments

--- Tugg/Tugg.ino
+++ Tugg/Tugg.ino
@@ -9,8 +9,8 @@
 
 #define STOP_DISTANCE 50 //cm
 
-#define RECEIVER_CH1_INPUT  3 //motor left
-#define RECEIVER_CH2_INPUT 5 //motor right
+#define RECEIVER_CH1_INPUT  3 //motor forward backward
+#define RECEIVER_CH2_INPUT 5 //motor left right
 
 #define RECEIVER_CH1_OUTPUT  9
 #define RECEIVER_CH2_OUTPUT  6
@@ -50,7 +50,7 @@ void connect_reciever(int input_ch, int output_ch){
 
 void loop() {
   connect_reciever(RECEIVER_CH1_INPUT, RECEIVER_CH1_OUTPUT); //reads the signal in, and reconstructs with the same duty cycle
-  // connect_reciever(RECEIVER_CH2_INPUT, RECEIVER_CH2_OUTPUT); //TODO will have to rework the connect_reciever method for limiting front movement
+  //the ch1 output is motor forward and backward, (the mdds will convert the wave into directions for the motor) we don't need t
 
   int dist = ultrasonic.read(CM); //TODO what to do when getting a whack value, add a filter to track the change in values, and have it in a reasonable range
   // Serial.println(dist);
