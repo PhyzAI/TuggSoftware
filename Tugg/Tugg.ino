ab9f8a0b44cd7babcf120ef828d48a8cc9b01e34
Yodaman07 <ayaan.irshad@gmail.com>
2025-07-22 15:15:41 -0700

    input pinmodes

--- Tugg/Tugg.ino
+++ Tugg/Tugg.ino
@@ -19,8 +19,13 @@ Ultrasonic ultrasonic(ULTRASONIC_TRIGGER_PIN, ULTRASONIC_ECHO_PIN);
 
 void setup() {
   //TODO: if the sensor is touching something else, it will output really high, incorrect numbers
-
   Serial.begin(9600); 
+  pinMode(RECEIVER_CH1_INPUT, INPUT);
+  pinMode(RECEIVER_CH2_INPUT, INPUT);
+
+  pinMode(RECEIVER_CH1_OUTPUT, OUTPUT);
+  pinMode(RECEIVER_CH2_OUTPUT, OUTPUT);
+
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(RELAY_SWITCH_PIN, OUTPUT);
   pinMode(ULTRASONIC_ECHO_PIN, INPUT_PULLUP); //if nothing is connected set input to high by default
@@ -30,6 +35,7 @@ void setup() {
   //We want pin 2 to be LOW regularly, and pin 2 to be HIGH if it gets too close
   //This is because a high signal will activate the relay which will keep the switches active. When a low signal it sent, the switches will release, and power will be cut to the motors
 }
+
 
 void connect_reciever(int input_ch, int output_ch){
   float t_high = pulseIn(input_ch, HIGH);
